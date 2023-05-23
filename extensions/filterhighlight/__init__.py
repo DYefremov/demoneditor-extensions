8c857ec20b8bc8e311989ca76ae50e5f0ef91fcf
DYefremov <dmitry.v.yefremov@gmail.com>
2023-05-23 13:11:05 +0300

    filterhighlight-> EPG tab support

--- extensions/filterhighlight/__init__.py
+++ extensions/filterhighlight/__init__.py
@@ -27,9 +27,9 @@
 
 import re
 
-from gi.repository import GLib, Gtk
+from gi.repository import Gtk
 
-from app.ui.uicommons import Page, Column
+from app.ui.uicommons import Column
 from extensions import BaseExtension
 
 
@@ -39,29 +39,38 @@ class Filterhighlight(BaseExtension):
 
     def __init__(self, app):
         super().__init__(app)
-        self._bq_inited = False
 
         style_context = self.app.services_view.get_style_context()
         self._color = style_context.get_color(Gtk.StateFlags.LINK).to_color().to_string()
 
-        app.connect("page-changed", self.on_page_changed)
+        app._stack_services_frame.connect("realize", self.on_bq_tab_realize)
+        app._stack_epg_box.connect("realize", self.on_epg_tab_realize)
 
-    def on_page_changed(self, app, page):
-        if not self._bq_inited and page is Page.SERVICES:
-            GLib.idle_add(self.init_bq_tab)
-            self.log(page)
-
-    def init_bq_tab(self):
+    def on_bq_tab_realize(self, widget):
         # Sat
         sat_entry = self.app._filter_entry
         view = self.app.services_view
         self.init_data_func(view, 2, sat_entry, Column.SRV_SERVICE, 1)  # Service
         self.init_data_func(view, 3, sat_entry, Column.SRV_PACKAGE, 0)  # Package
-
         # IPTV
         iptv_entry = self.app._iptv_filter_entry
         view = self.app.iptv_services_view
         self.init_data_func(view, 0, iptv_entry, Column.IPTV_SERVICE, 0)  # Service
+
+    def on_epg_tab_realize(self, widget):
+        from app.ui.epg.epg import EpgTool
+
+        children = widget.get_children()
+        if not children or not isinstance(children[0], EpgTool):
+            self.log("Initialization error [EPG tab].")
+            return
+
+        tool = children[0]
+        view = tool._view
+        entry = tool._filter_entry
+        self.init_data_func(view, 0, entry, 0, 0)  # Service
+        self.init_data_func(view, 1, entry, 1, 0)  # Title
+        self.init_data_func(view, 5, entry, 5, 0)  # Description
 
     def init_data_func(self, view, column, entry, m_index, cel):
         v_column = view.get_column(column)
